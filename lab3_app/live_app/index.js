f330f7ae11dc332c0b339c0b0c1e822608a856d3
Nkosana <1439120@students.wits.ac.za>
2020-04-21 10:23:15 +0200

    Exercise 1

--- lab3_app/live_app/index.js
+++ lab3_app/live_app/index.js
@@ -6,3 +6,25 @@ button.addEventListener('click', function () {
   paragraph.appendChild(text) // Append the text to <p>
   document.body.appendChild(paragraph) // Append <p> to <body>
 }, false)
+
+button.addEventListener('click', function () {
+  const header = document.createElement('h5')
+  header.title = 'Heading for adding a link'
+
+  const p = document.createElement('p')
+  const mssg = document.createTextNode('Go to this link to access dummy ')
+  p.appendChild(mssg)
+
+  const a = document.createElement('a')
+  const link = document.createTextNode('Link dummy')
+  a.appendChild(link)
+  a.href = 'dummy.html'
+
+  p.appendChild(a)
+  document.body.appendChild(p)
+}, true)
+
+const p = document.createElement('p')
+const mssg = document.createTextNode('Message before u press the button button')
+p.appendChild(mssg)
+document.body.appendChild(p)
