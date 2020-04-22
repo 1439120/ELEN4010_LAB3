0d868608b668d061a93cfe567466df79e7f6e8ff
Nkosana <1439120@students.wits.ac.za>
2020-04-22 21:13:57 +0200

    Exercise 2

--- lab3_app/live_app/index.js
+++ lab3_app/live_app/index.js
@@ -1,30 +1,66 @@
-const button = document.getElementById('addButton')
+/** ***********   These are my students ****************** */
+let students = [{ name: 'Kwezi', studentNumber: 453528 },
+  { name: 'Pieter', studentNumber: 454345 },
+  { name: 'Jade', studentNumber: 678345 },
+  { name: 'Kiren', studentNumber: 567893 },
+  { name: 'Nkosana', studentNumber: 1439120 }]
 
-button.addEventListener('click', function () {
-  const paragraph = document.createElement('p') // Create <p> element
-  const text = document.createTextNode('This is a student') // Create text node
-  paragraph.appendChild(text) // Append the text to <p>
-  document.body.appendChild(paragraph) // Append <p> to <body>
-}, false)
+/** *********** First button for Lists */
+const List_students = document.getElementById('addButton')
+List_students.innerHTML = 'List'
 
-button.addEventListener('click', function () {
-  const header = document.createElement('h5')
-  header.title = 'Heading for adding a link'
+/** ***********  button for Erasing */
+const remove_all = document.createElement('button')
+remove_all.innerHTML = 'Erase'
 
-  const p = document.createElement('p')
-  const mssg = document.createTextNode('Go to this link to access dummy ')
-  p.appendChild(mssg)
+/** ****************Button that displays students **********/
 
-  const a = document.createElement('a')
-  const link = document.createTextNode('Link dummy')
-  a.appendChild(link)
-  a.href = 'dummy.html'
+function myfunction () {
+  const my_students = document.getElementById('studentList')
+  const paragraph = document.createElement('p')
 
-  p.appendChild(a)
-  document.body.appendChild(p)
-}, true)
+  students.forEach(function (item) {
+    // Each student should have a seperate button
+    const student_button = document.createElement('button')
+    student_button.innerHTML = 'Edit'
 
-const p = document.createElement('p')
-const mssg = document.createTextNode('Message before u press the button button')
-p.appendChild(mssg)
-document.body.appendChild(p)
+    const text = document.createTextNode(' Name:  ' + item.name + ' - student no: ' + item.studentNumber)
+    // Onclick button
+    student_button.onclick = function () {
+      const new_name = prompt('Write new name of student')
+      if (new_name.length != null) {
+        item.name = new_name
+      }
+      const new_std_no = prompt('Write new student number')
+      if (new_std_no != null && !isNaN(new_std_no)) {
+        item.studentNumber = new_std_no
+      }
+      paragraph.parentNode.removeChild(paragraph)
+      myfunction()
+    }
+
+    paragraph.appendChild(student_button)
+    paragraph.appendChild(text)
+    paragraph.appendChild(document.createElement('br'))
+  })
+  my_students.appendChild(paragraph)
+
+  /** ******************* Delete List ***** */
+  if (students.length != 0) {
+    remove_all.onclick = function () {
+      students = []
+      remove_all.parentNode.removeChild(remove_all)
+      paragraph.parentNode.removeChild(paragraph)
+    }
+    document.body.appendChild(remove_all)
+  }
+}
+// }, true)
+var yes = true
+function all_students () {
+  if (!yes) return
+  yes = false
+  myfunction()
+}
+
+List_students.onclick = all_students
