3bd6d587bd205e15912ed528b424bd876b3b785e
Nkosana <1439120@students.wits.ac.za>
2020-05-08 12:01:14 +0200

    Source code from lab2

---
+++ Back-end-Development/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const mainRouter = require('./mainRoutes.js')
+
+app.use(mainRouter)
+app.listen(3000)
+console.log('Express server running on port 3000')
